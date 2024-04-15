c96e90db221ca017f384548b1f34c1094dc2e0cf
shammy46 <sanjidashammy81@gmail.com>
2024-04-16 00:09:28 +0600

    left side of home added

--- src/pages/shared/LeftNav/LeftNav.jsx
+++ src/pages/shared/LeftNav/LeftNav.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftNav = () => {
+    const [categories, setCategories]= useState([]);
+
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res=>res.json())
+        .then(data=>setCategories(data));
+
+    },[]);
+
+
+
+
     return (
-        <div>
-            <h2 className="text-3xl">leftnav</h2>
-            
+        <div className="space-y-6">
+            <h2 className="text-3xl">All Categories</h2>
+            {
+                categories.map(category=> <Link
+                className="block ml-4 text-xl font-semibold"
+                     key={category.id}
+                     to={`/category/${category.id}`}
+                     >{category.name}</Link>)
+            }
         </div>
     );
 };
